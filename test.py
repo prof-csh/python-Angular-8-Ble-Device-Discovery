d1e1588e14b8b736b9a546090200a13cd7459983
prof-csh <prof.csh@gmail.com>
2019-11-27 22:33:53 -0800

    1 file modified

--- test.py
+++ test.py
@@ -1,84 +1,124 @@
-# Example of interaction with a BLE UART device using a UART service
-# implementation.
-# Author: Tony DiCola
+import time
+import uuid
+import random
 import Adafruit_BluefruitLE
-from Adafruit_BluefruitLE.services import UART
-
-
+ 
+DEVICE_NAME = "Blank"
+ 
+# Define service and characteristic UUIDs used by the peripheral.
+SERVICE_UUID = uuid.UUID('00001111-0000-1000-8000-00805F9B34FB')
+TX_CHAR_UUID = uuid.UUID('00002222-0000-1000-8000-00805F9B34FB')
+RX_CHAR_UUID = uuid.UUID('00002222-0000-1000-8000-00805F9B34FB')
+ 
 # Get the BLE provider for the current platform.
 ble = Adafruit_BluefruitLE.get_provider()
-
-
-# Main function implements the program logic so it can run in a background
-# thread.  Most platforms require the main thread to handle GUI events and other
-# asyncronous events like BLE actions.  All of the threading logic is taken care
-# of automatically though and you just need to provide a main function that uses
-# the BLE provider.
-def main():
-    # Clear any cached data because both bluez and CoreBluetooth have issues with
-    # caching data and it going stale.
-    ble.clear_cached_data()
-
-    # Get the first available BLE network adapter and make sure it's powered on.
-    adapter = ble.get_default_adapter()
-    adapter.power_on()
-    print('Using adapter: {0}'.format(adapter.name))
-
-    # Disconnect any currently connected UART devices.  Good for cleaning up and
-    # starting from a fresh state.
-    print('Disconnecting any connected UART devices...')
-    UART.disconnect_devices()
-
-    # Scan for UART devices.
-    print('Searching for UART device...')
+ 
+ 
+def scan_for_peripheral(adapter):
+    """Scan for BLE peripheral and return device if found"""
+    print('  Searching for device...')
     try:
         adapter.start_scan()
-        # Search for the first UART device found (will time out after 60 seconds
+        # Scan for the peripheral (will time out after 60 seconds
         # but you can specify an optional timeout_sec parameter to change it).
-        device = UART.find_device()
+        device = ble.find_device(name=DEVICE_NAME)
         if device is None:
-            raise RuntimeError('Failed to find UART device!')
+            raise RuntimeError('Failed to find device!')
+        return device
     finally:
         # Make sure scanning is stopped before exiting.
         adapter.stop_scan()
-
-    print('Connecting to device...')
-    device.connect()  # Will time out after 60 seconds, specify timeout_sec parameter
-                      # to change the timeout.
-
-    # Once connected do everything else in a try/finally to make sure the device
-    # is disconnected when done.
+ 
+ 
+def sleep_random(min_ms=1, max_ms=1000):
+    """Add a random sleep interval between 1ms to 1000ms"""
+    duration_sec = random.randrange(min_ms, max_ms)/1000
+    print('   Sleeping for ' + str(duration_sec) + 'sec')
+    time.sleep(duration_sec)
+ 
+ 
+def main():
+    """Main loop to process BLE events"""
+    test_iteration = 0
+    echo_mismatch_count = 0
+    misc_error_count = 0
+ 
+    # Clear any cached data because both BlueZ and CoreBluetooth have issues with
+    # caching data and it going stale.
+    ble.clear_cached_data()
+ 
+    # Get the first available BLE network adapter and make sure it's powered on.
+    adapter = ble.get_default_adapter()
     try:
-        # Wait for service discovery to complete for the UART service.  Will
-        # time out after 60 seconds (specify timeout_sec parameter to override).
-        print('Discovering services...')
-        UART.discover(device)
-
-        # Once service discovery is complete create an instance of the service
-        # and start interacting with it.
-        uart = UART(device)
-
-        # Write a string to the TX characteristic.
-        uart.write(b'Hello world!\r\n')
-        print("Sent 'Hello world!' to the device.")
-
-        # Now wait up to one minute to receive data from the device.
-        print('Waiting up to 60 seconds to receive data from the device...')
-        received = uart.read(timeout_sec=60)
-        if received is not None:
-            # Received data, print it out.
-            print('Received: {0}'.format(received))
-        else:
-            # Timeout waiting for data, None is returned.
-            print('Received no data!')
-    finally:
-        # Make sure device is disconnected on exit.
-        device.disconnect()
-
-
+        adapter.power_on()
+        print('Using adapter: {0}'.format(adapter.name))
+ 
+        # This loop contains the main logic for testing the BLE peripheral.
+        # We scan and connect to the peripheral, discover services,
+        # read/write to characteristics, and keep track of errors.
+        # This test repeats 10 times.
+        while test_iteration < 10:
+            connected_to_peripheral = False
+ 
+            while not connected_to_peripheral:
+                try:
+                    peripheral = scan_for_peripheral(adapter)
+                    peripheral.connect(timeout_sec=10)
+                    connected_to_peripheral = True
+                    test_iteration += 1
+                    print('-- Test iteration #{} --'.format(test_iteration))
+                except BaseException as e:
+                    print("Connection failed: " + str(e))
+                    time.sleep(1)
+                    print("Retrying...")
+ 
+            try:
+                print('  Discovering services and characteristics...')
+                peripheral.discover([SERVICE_UUID], [TX_CHAR_UUID, RX_CHAR_UUID])
+ 
+                # Find the service and its characteristics
+                service = peripheral.find_service(SERVICE_UUID)
+                tx = service.find_characteristic(TX_CHAR_UUID)
+                rx = service.find_characteristic(RX_CHAR_UUID)
+ 
+                # Randomize the intervals between different operations
+                # to simulate user-triggered BLE actions.
+                sleep_random(1, 1000)
+ 
+                # Write random value to characteristic.
+                write_val = bytearray([random.randint(1, 255)])
+                print('  Writing ' + str(write_val) + ' to the write char')
+                tx.write_value(write_val)
+ 
+                sleep_random(1, 1000)
+ 
+                # Read characteristic and make sure it matches the value written.
+                read_val = rx.read_value()
+                print('  Read ' + str(read_val) + ' from the read char')
+                if write_val != read_val:
+                    echo_mismatch_count = echo_mismatch_count + 1
+                    print('  Read value does not match value written')
+ 
+                peripheral.disconnect()
+                sleep_random(1, 1000)
+            except BaseException as e:
+                misc_error_count = misc_error_count + 1
+                print('Unexpected error: ' + str(e))
+                print('Current error count: ' + str(misc_error_count))
+                time.sleep(1)
+                print('Retrying...')
+   
+ finally:
+        # Disconnect device on exit.
+        peripheral.disconnect
+        print('\nConnection count: ' + str(test_iteration))
+        print('Echo mismatch count: ' + str(echo_mismatch_count))
+        print('Misc error count: ' + str(misc_error_count))
+ 
+ 
 # Initialize the BLE system.  MUST be called before other BLE calls!
 ble.initialize()
-
+ 
 # Start the mainloop to process BLE events, and run the provided function in
 # a background thread.  When the provided main function stops running, returns
 # an integer status code, or throws an error the program will exit.
